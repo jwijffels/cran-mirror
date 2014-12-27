db91af350425892cbbb3639196d3de71a6649d0c
Jan Wijffels <jwijffels@bnosac.be>
2014-12-27 23:53:08 +0100

    Rstudio shit

--- r-common/userconf.sh
+++ r-common/userconf.sh
@@ -6,13 +6,14 @@ EMAIL=${EMAIL:=r-common@example.com}
 USERID=${USERID:=1000}
 ROOT=${ROOT:=TRUE}
 
+userdel docker
 useradd -m $USER -u $USERID 
 echo "$USER:$PASSWORD" | chpasswd
 mkdir /home/$USER 
 chown $USER:$USER /home/$USER
 
 ## Make sure user has write access to /usr/local/lib/R/site.library
-addgroup $USER root
+addgroup $USER staff
 
 ## Configure git 
 echo -e "[user]\n\tname = $USER\n\temail = $EMAIL\n\n[credential]\n\thelper = cache\n\n[push]\n\tdefault = simple\n\n[core]\n\teditor = vim\n" > /home/$USER/.gitconfig
