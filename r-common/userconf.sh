f4f92e24a473d3c01e5a38271ac99bbd76d142da
Jan Wijffels <jwijffels@bnosac.be>
2014-12-27 23:07:05 +0100

    Added root & sudo

--- r-common/userconf.sh
+++ r-common/userconf.sh
@@ -4,7 +4,7 @@ USER=${USER:=rstudio}
 PASSWORD=${PASSWORD:=rstudio}
 EMAIL=${EMAIL:=r-common@example.com}
 USERID=${USERID:=1000}
-ROOT=${ROOT:=FALSE}
+ROOT=${ROOT:=TRUE}
 
 useradd -m $USER -u $USERID 
 echo "$USER:$PASSWORD" | chpasswd
@@ -12,9 +12,16 @@ mkdir /home/$USER
 chown $USER:$USER /home/$USER
 
 ## Make sure user has write access to /usr/local/lib/R/site.library
-addgroup $USER staff
+addgroup $USER root
 
 ## Configure git 
 echo -e "[user]\n\tname = $USER\n\temail = $EMAIL\n\n[credential]\n\thelper = cache\n\n[push]\n\tdefault = simple\n\n[core]\n\teditor = vim\n" > /home/$USER/.gitconfig
 
+## Global config make page-up/down search history
+## echo '"\e[5~": history-search-backward' >> /etc/inputrc && echo '"\e[6~": history-search-backward' >> /etc/inputrc
+
+# Use Env flag to know if user should be added to sudoers
+if [ "$ROOT" == "TRUE" ]; then
+  adduser $USER sudo && echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+fi
 
